58408fb44cf7043310cf6fa571f75703364d2941
namste gupta <77376210+tcg2710@users.noreply.github.com>
2024-07-04 19:19:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = environ.get("API_ID", "18688563")
     API_HASH = environ.get("API_HASH", "48315c25585c23eee6b1f346aad17a68")
     BOT_TOKEN = environ.get("BOT_TOKEN", "6786278075:AAHvQDFYEUk68NtJ95uAHyLKCC02afDLjpo") 
-    BOT_SESSION = environ.get("BOT_SESSION", "BQCTxF_JKRSCREkZLNiVLfFyERGYvqML1dEu95KHqbHgNSCzDV_RqcjEV9iE6Z68ZjjdwEj0dZfinpod4yEKM2uXgjntZ56qY5rkGRZYN5tv4Ul-SQOUVyQQWdWRKjVUNZWNLTEzt1AoMkrB3WcsMfzL1FChNYhRPsQpaqPhAmsFJ_ZNE5psZ32GPm9ro566c2jYdrzfd74HNUHM08uHrq1SBS7Q9ktmTUkE26558qQwEMki8X6MPLmjf28hf7OYdXnbCLj1H2HIiwb2sAra9dbT3QmNa9K9kDCY6F36zFUtIKEG3tpG94zVvwQ8r_MAUaQCx_CtIL0Wi0rsyq-oOqn_AAAAAUuSxxUA") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5562877717').split()]
